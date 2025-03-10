7609b54a1bb6f713974cb0121aaab5a94abdeb66
Zawadi Brown <zawadi_sb@yahoo.com>
2025-03-09 21:48:07 -0400

    server

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -1,12 +1,16 @@
 import path from 'node:path';
+import { fileURLToPath } from 'url';
 import { Router } from 'express';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const router = Router();
 
-
 router.get('/', (_, res) => {
-  res.sendFile(path.resolve('client/public/src/index.html'));
+  res.sendFile(path.resolve(__dirname, '../../client/public/src/index.html'));
 });
+
 
 
 export default router;
